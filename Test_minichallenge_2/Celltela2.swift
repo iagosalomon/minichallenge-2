2a9635b613bda48e47e9292f1bb03d30d796ca70
MarianaLima <marianalima@10-44-48-185.0200.mack.br.iosda.org>
2019-05-14 17:41:27 -0300

    tela 2 feita

---
+++ Test_minichallenge_2/Celltela2.swift
@@ -0,0 +1,17 @@
+//
+//  Celltela2.swift
+//  Test_minichallenge_2
+//
+//  Created by Mariana Lima on 13/05/19.
+//  Copyright © 2019 iago salomon. All rights reserved.
+//
+
+import UIKit
+
+class Celltela2: UICollectionViewCell {
+    
+    @IBOutlet weak var lablelTela2Cell: UILabel!
+    @IBOutlet weak var imagetela2: UIImageView!
+    @IBOutlet weak var botao: UIButton!
+}
+
